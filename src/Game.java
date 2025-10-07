4a7fbc32f7048a2ff33910de7dcb6c5a49d49ddd
Mohamed Mehdi Abid <154626512+AdlerAB@users.noreply.github.com>
2025-10-07 15:08:25 +0200

    add player

--- src/Game.java
+++ src/Game.java
@@ -26,6 +26,9 @@ class GamePanel extends JPanel implements ActionListener, KeyListener {
     private GameState state = GameState.START;
     private boolean enterPressed = false;
 
+    // === NOUVEAU : joueur ===
+    private final Player player = new Player(380, 500);
+
     public GamePanel() {
         setPreferredSize(new Dimension(800, 600));
         setBackground(Color.black);
@@ -46,13 +49,12 @@ class GamePanel extends JPanel implements ActionListener, KeyListener {
                 if (enterPressed) state = GameState.RUNNING;
                 break;
             case RUNNING:
-                // TODO: update player, bullets, enemies, collisions, HUD
+                // === NOUVEAU : mise à jour du joueur ===
+                player.update();
                 break;
             case LEVEL_COMPLETE:
-                // TODO: transition to next level
                 break;
             case GAME_OVER:
-                // TODO: R to restart
                 break;
         }
     }
@@ -66,7 +68,8 @@ class GamePanel extends JPanel implements ActionListener, KeyListener {
                 break;
             case RUNNING:
                 g.drawString("HUD: score=0 lives=3 level=1", 10, 20);
-                // TODO: draw world, player, enemies, bullets
+                // === NOUVEAU : dessin du joueur ===
+                player.draw(g);
                 break;
             case LEVEL_COMPLETE:
                 drawCentered(g, "LEVEL COMPLETE!");
@@ -85,9 +88,68 @@ class GamePanel extends JPanel implements ActionListener, KeyListener {
     }
 
     @Override public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) enterPressed = true;
-        // TODO: handle arrows/WASD/space in RUNNING state
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_ENTER) enterPressed = true;
+
+        // === NOUVEAU : contrôles quand le jeu tourne ===
+        if (state == GameState.RUNNING) {
+            player.keyPressed(code);
+        }
     }
-    @Override public void keyReleased(KeyEvent e) {}
+
+    @Override public void keyReleased(KeyEvent e) {
+        if (state == GameState.RUNNING) {
+            player.keyReleased(e.getKeyCode());
+        }
+    }
+
     @Override public void keyTyped(KeyEvent e) {}
+}
+
+// ===================
+// Classe Player simple
+// ===================
+class Player {
+    private int x, y, width, height, speed;
+    private boolean up, down, left, right;
+
+    public Player(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+        this.width = 40;
+        this.height = 40;
+        this.speed = 5;
+    }
+
+    public void update() {
+        if (up) y -= speed;
+        if (down) y += speed;
+        if (left) x -= speed;
+        if (right) x += speed;
+
+        // Limites écran 800x600
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+        if (x + width > 800) x = 800 - width;
+        if (y + height > 600) y = 600 - height;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(x, y, width, height);
+    }
+
+    public void keyPressed(int keyCode) {
+        if (keyCode == KeyEvent.VK_W || keyCode == KeyEvent.VK_UP)    up = true;
+        if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN)  down = true;
+        if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_LEFT)  left = true;
+        if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT) right = true;
+    }
+
+    public void keyReleased(int keyCode) {
+        if (keyCode == KeyEvent.VK_W || keyCode == KeyEvent.VK_UP)    up = false;
+        if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN)  down = false;
+        if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_LEFT)  left = false;
+        if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT) right = false;
+    }
 }
